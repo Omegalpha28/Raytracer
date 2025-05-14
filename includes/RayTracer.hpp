ec0dc624945f383f692bc9aba4232936af424569
GnXloan <loan.badey@epitech.eu>
2025-05-14 14:10:55 +0200

    [ADD] colors

--- includes/RayTracer.hpp
+++ includes/RayTracer.hpp
@@ -10,6 +10,42 @@
 
 namespace RayTracer {
 
+    class color {
+        public:
+            color();
+            ~color();
+            color(int r, int g, int b, int a);
+            int getR(void) {
+                return _r;
+            };
+            int getG(void) {
+                return _g;
+            };
+            int getB(void) {
+                return _b;
+            };
+            int getA(void) {
+                return _a;
+            };
+            void setR(int r) {
+                _r = r;
+            };
+            void setG(int g) {
+                _g = g;
+            };
+            void setB(int b) {
+                _b = b;
+            };
+            void setA(int a) {
+                _a = a;
+            };
+        private:
+            int _r;
+            int _g;
+            int _b;
+            int _a;
+    };
+
     class Ray {
         public:
             Ray();
@@ -33,6 +69,16 @@ namespace RayTracer {
         private:
             Math::Point3D _center;
             double _radius;
+            color _color;
+    };
+
+    class Light {
+        public:
+            Light();
+            ~Light();
+        private:
+            Math::Point3D _position;
+            color _color;
     };
 
     class Rectangle3D {
@@ -45,6 +91,6 @@ namespace RayTracer {
             Math::Point3D _origin;
             Math::Vector3D _bottom_side;
             Math::Vector3D _left_side;
-
+            color _color;
     };
 };
