c14669e3860eb5b8958057057a8002f9f57b9e03
GnXloan <loan.badey@epitech.eu>
2025-05-14 15:58:16 +0200

    [DEL] text.cpp

--- Text.cpp
+++
@@ -1,9 +0,0 @@
-/*
-** EPITECH PROJECT, 2024
-** B-OOP-400-LYN-4-1-raytracer-mathis.quenardel
-** File description:
-** Text.cpp
-*/
-
-#include "../includes/includes.hpp"
-#include "../includes/RayTracer.hpp"
